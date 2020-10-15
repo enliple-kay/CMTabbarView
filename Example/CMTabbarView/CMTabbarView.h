3ab74f3ce70853cc576b5f78844a2ea820507d52
kay <ickang@enliple.com>
2020-10-15 16:14:13 +0900

    Fixed

---
+++ Example/CMTabbarView/CMTabbarView.h
@@ -0,0 +1,19 @@
+//
+//  CMTabbarView.h
+//  CMTabbarView
+//
+//  Created by kay on 2020/10/15.
+//  Copyright © 2020 momo605654602@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CMTabbarView.
+FOUNDATION_EXPORT double CMTabbarViewVersionNumber;
+
+//! Project version string for CMTabbarView.
+FOUNDATION_EXPORT const unsigned char CMTabbarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMTabbarView/PublicHeader.h>
+
+
